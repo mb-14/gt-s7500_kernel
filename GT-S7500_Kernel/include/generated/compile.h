ae5947e1ea922af8074bc025c37bef5cafcfeebc
blackburner <e.luis.lf@gmail.com>
2012-05-27 16:35:24 +0100

    first commit

---
+++ GT-S7500_Kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 6 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 PREEMPT Sun May 27 12:28:38 WEST 2012"
+#define LINUX_COMPILE_TIME "12:28:38"
+#define LINUX_COMPILE_BY "blackburner"
+#define LINUX_COMPILE_HOST "clunker"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
