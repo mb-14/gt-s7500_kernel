c060a9e1abee8c08441c8194a57515b89a276274
Miroojin Bakshi <gaglu231094@gmail.com>
2012-05-30 20:13:00 -0700

    remove non google governors

--- GT-S7500_Kernel/include/generated/compile.h
+++ GT-S7500_Kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 16 */
+/* This file is auto generated, version 17 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#16 PREEMPT Tue May 29 21:32:34 MST 2012"
-#define LINUX_COMPILE_TIME "21:32:34"
+#define UTS_VERSION "#17 PREEMPT Tue May 29 23:16:27 MST 2012"
+#define LINUX_COMPILE_TIME "23:16:27"
 #define LINUX_COMPILE_BY "mb-14"
 #define LINUX_COMPILE_HOST "localhost"
 #define LINUX_COMPILE_DOMAIN "(none)"
