fd65911aa905213077d7caf7e9caa98b60e46bfb
Miroojin Bakshi <gaglu231094@gmail.com>
2012-05-31 22:46:58 -0700

    derp

--- initramfs/init.qcom.post_boot.sh
+++ initramfs/init.qcom.post_boot.sh
@@ -28,7 +28,7 @@
 
 target=`getprop ro.product.device`
 case "$target" in
-    "GT-S6202"| "GT-S7500" | "GT-S6500" | "GT-S7509" | "SGH-I827" | "msm7201a_ffa" | "msm7201a_surf" | "msm7627_ffa" | "msm7627_surf" | "msm7627a" | \
+    "GT-S6202"| "GT-S7500" | "GT-S6500" | "GT-S6500D" | "GT-S7509" | "SGH-I827" | "msm7201a_ffa" | "msm7201a_surf" | "msm7627_ffa" | "msm7627_surf" | "msm7627a" | \
     "qsd8250_surf" | "qsd8250_ffa" | "msm7630_surf" | "msm7630_1x" | "msm7630_fusion" | "qsd8650a_st1x")
         echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
         echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
@@ -42,99 +42,21 @@ case "$target" in
         ;;
 esac
 
+
 case "$target" in
-    "msm7630_surf" | "msm7630_1x" | "msm7630_fusion")
-        echo 75000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-        echo 1 > /sys/module/pm2/parameters/idle_sleep_mode
+        chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
         ;;
 esac
 
-case "$target" in
-     "GT-S6202" | "GT-S7500" | "GT-S6500" | "GT-S7509" | "SGH-I827" | "msm7201a_ffa" | "msm7201a_surf" | "msm7627_ffa" | "msm7627_surf" | "msm7630_surf" | "msm7630_1x" | "msm7630_fusion" | "msm7627a" )
-        echo 245760 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        ;;
-esac
+
 
 case "$target" in
-    "msm8660_surf" | "msm8660_csfb")
-	 echo 1 > /sys/module/rpm_resources/enable_low_power/L2_cache
-	 echo 1 > /sys/module/rpm_resources/enable_low_power/pxo
-	 echo 2 > /sys/module/rpm_resources/enable_low_power/vdd_dig
-	 echo 2 > /sys/module/rpm_resources/enable_low_power/vdd_mem
-	 echo 1 > /sys/module/rpm_resources/enable_low_power/rpm_cpu
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/suspend_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/idle_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
-	 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
-	 echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-	 echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-	 echo 50000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-	 echo 50000 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
-	 echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
-	 echo 90 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/up_threshold
-	 echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-	 echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-	 chown system /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-	 chown system /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
-	 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-	 echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-	 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-	 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-	 chown root.system /sys/devices/system/cpu/mfreq
-	 chmod 220 /sys/devices/system/cpu/mfreq
-	 chown root.system /sys/devices/system/cpu/cpu1/online
-	 chmod 664 /sys/devices/system/cpu/cpu1/online
-        ;;
-esac
-
-case "$target" in
-    "msm8960")
-     echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-     echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-     echo 50000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-     echo 50000 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
-     echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/up_threshold
-     echo 90 > /sys/devices/system/cpu/cpu1/cpufreq/ondemand/up_threshold
-     echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
-     echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
-     chown system /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-     chown system /sys/devices/system/cpu/cpu1/cpufreq/ondemand/sampling_rate
-     echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-     echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-     chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-     chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-     chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
-     chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-     chown root.system /sys/devices/system/cpu/mfreq
-     chmod 220 /sys/devices/system/cpu/mfreq
-     chown root.system /sys/devices/system/cpu/cpu1/online
-     chmod 664 /sys/devices/system/cpu/cpu1/online
-        ;;
-esac
-
-case "$target" in
-    "GT-S6202" | "msm7627_ffa" | "msm7627_surf" | "GT-S7500" | "GT-S6500" | "GT-S7509" | "SGH-I827" | "msm7627a")
+    "GT-S6202" | "msm7627_ffa" | "msm7627_surf" | "GT-S7500" | "GT-S6500" | "GT-S6500D" | "GT-S7509" | "SGH-I827" | "msm7627a")
         echo 25000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
         ;;
 esac
 
-case "$target" in
-    "qsd8250_surf" | "qsd8250_ffa" | "qsd8650a_st1x")
-        echo 50000 > /sys/devices/system/cpu/cpu0/cpufreq/ondemand/sampling_rate
-        ;;
-esac
-
-case "$target" in
-    "qsd8650a_st1x")
-        mount -t debugfs none /sys/kernel/debug
-    ;;
-esac
 
 emmc_boot=`getprop ro.emmc`
 case "$emmc_boot"
