e7f5a7e3100e2c057224c846937490f928514b8e
Miroojin Bakshi <gaglu231094@gmail.com>
2012-06-08 04:08:55 -0700

    Enable Zram acpuclock-overclock changes

--- GT-S7500_Kernel/include/generated/compile.h
+++ GT-S7500_Kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 21 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#20 PREEMPT Thu May 31 22:29:43 MST 2012"
-#define LINUX_COMPILE_TIME "22:29:43"
+#define UTS_VERSION "#21 PREEMPT Thu May 31 22:53:16 MST 2012"
+#define LINUX_COMPILE_TIME "22:53:16"
 #define LINUX_COMPILE_BY "mb-14"
 #define LINUX_COMPILE_HOST "localhost"
 #define LINUX_COMPILE_DOMAIN "(none)"
