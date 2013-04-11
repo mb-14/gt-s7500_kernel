e97426d085c1d73e259f5abec3c828d4e3f894eb
mb-14 <mb-14@iof303.pnap.ioflood.com>
2013-04-11 09:32:27 -0700

    Updates: Stable OC SIO scheduler

--- GT-S7500_Kernel/include/generated/compile.h
+++ GT-S7500_Kernel/include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 44 */
+/* This file is auto generated, version 45 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#44 PREEMPT Wed Oct 31 07:24:51 MST 2012"
-#define LINUX_COMPILE_TIME "07:24:51"
+#define UTS_VERSION "#45 PREEMPT Thu Apr 11 09:08:55 MST 2013"
+#define LINUX_COMPILE_TIME "09:08:55"
 #define LINUX_COMPILE_BY "mb-14"
-#define LINUX_COMPILE_HOST "gooey.goo.im"
-#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILE_HOST "iof303"
+#define LINUX_COMPILE_DOMAIN "pnap.ioflood.com"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
